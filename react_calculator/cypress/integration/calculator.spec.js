3bcc9c136622e9f72d588fc2ad34c03e47fc5d72
elicodes89 <elicodes89@gmail.com>
2022-05-02 12:12:41 +0100

    added new test

--- react_calculator/cypress/integration/calculator.spec.js
+++ react_calculator/cypress/integration/calculator.spec.js
@@ -80,26 +80,22 @@ describe("Calculator", () => {
 
   it('should display large numbers' , () => {
     cy.get('#number2').click();
-    cy.get('#operator_add').click();
-    cy.get('#number3').click();
-    cy.get('#operator-divide').click();
-    cy.get('#number9').click();
-    cy.get('#operator-subtract').click();
-    cy.get('#number8').click();
-    cy.get('#decimal').click();
-    cy.get('#number1').click();
-    cy.get('#number9').click();
     cy.get('#number2').click();
+    cy.get('#number4').click();
     cy.get('#operator_add').click();
-    cy.get('#number3').click();
-    cy.get('#number2').click();
-    cy.get('#operator_add').click();
-    cy.get('#number3').click();
-    cy.get('#operator-divide').click();
     cy.get('#number9').click();
-    cy.get('#operator-subtract').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number6').click();
+    cy.get('#operator-multiply').click();
     cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number9').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
     cy.get('#operator-equals').click();
-    cy.get('.display').should('contain','-4.959604938271605')
+    cy.get('.display').should('contain','9147855')
   })
 })
